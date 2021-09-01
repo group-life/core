3ac98a1a329546f1fe4a74f51bc823ebfc265c67
Lehersun <kpekep@list.ru>
2021-09-01 13:12:06 +0300

    Fix code style violations

--- tests/ScheduleTest.php
+++ tests/ScheduleTest.php
@@ -10,8 +10,8 @@ class ScheduleTest extends TestCase
 
     public function testMaterialize()
     {
-        $from = new \DateTime('8:00', new \DateTimeZone( '+0300' ));
-        $to = new \DateTime('8:00 +7 days', new \DateTimeZone( '+0300' ));
+        $from = new \DateTime('8:00', new \DateTimeZone('+0300'));
+        $to = new \DateTime('8:00 +7 days', new \DateTimeZone('+0300'));
 
 
         $schedule = new Schedule($from, $to);
