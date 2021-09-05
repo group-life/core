34c5df9aec993424f76e61d84ec68fcf542ef867
Lehersun <kpekep@list.ru>
2021-09-05 12:52:32 +0300

    Create test suite for DayRule

---
+++ tests/Schedule/DayRuleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GroupLife\Core\tests;
+
+use GroupLife\Core\Schedule\DayRule;
+use PHPUnit\Framework\TestCase;
+
+class DayRuleTest extends TestCase
+{
+
+    public function testIncludedDays()
+    {
+        $dateFrom = new \DateTime('2021-01-01');
+        $oneMonth = new \DateInterval('P1M');
+        $day = new DayRule(['2021-01-01' => '10:00', '2021-01-02' => '09:00']);
+        $this->assertEquals(
+            [
+                new \DateTime('2021-01-01 10:00'),
+                new \DateTime('2021-01-02 09:00'),
+            ],
+            $day->includedDays($dateFrom, $oneMonth)
+        );
+    }
+
+    public function testExcludedDays()
+    {
+        $dateFrom = new \DateTime('2021-01-01');
+        $oneMonth = new \DateInterval('P1M');
+        $day = new DayRule(['2021-01-01' => '10:00', '2021-01-02' => '09:00']);
+        $this->assertEquals(
+            [],
+            $day->excludedDays($dateFrom, $oneMonth)
+        );
+    }
+}
