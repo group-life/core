c634e14a629aaf10789425075e239d47670544b9
Lehersun <kpekep@list.ru>
2021-09-05 12:58:30 +0300

    Create test suite for CancelDayRule

---
+++ tests/Schedule/CancelDayRuleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GroupLife\Core\tests;
+
+use GroupLife\Core\Schedule\CancelDayRule;
+use PHPUnit\Framework\TestCase;
+
+class CancelDayRuleTest extends TestCase
+{
+
+    public function testIncludedDays()
+    {
+        $dateFrom = new \DateTime('2021-01-01');
+        $oneMonth = new \DateInterval('P1M');
+        $cancelDays = new CancelDayRule(['2021-01-01' => '10:00', '2021-01-02' => '09:00']);
+        $this->assertEquals(
+            [],
+            $cancelDays->includedDays($dateFrom, $oneMonth)
+        );
+    }
+
+    public function testExcludedDays()
+    {
+        $dateFrom = new \DateTime('2021-01-01');
+        $oneMonth = new \DateInterval('P1M');
+        $cancelDays = new CancelDayRule(['2021-01-01' => '10:00', '2021-01-02' => '09:00']);
+        $this->assertEquals(
+            [
+                new \DateTime('2021-01-01 10:00'),
+                new \DateTime('2021-01-02 09:00'),
+            ],
+            $cancelDays->excludedDays($dateFrom, $oneMonth)
+        );
+    }
+}
