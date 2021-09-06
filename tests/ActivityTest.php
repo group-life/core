5475a24d292470661246d52d92420f1084d15d45
Lehersun <kpekep@list.ru>
2021-09-06 16:34:53 +0300

    Fix using namespaces by tests

--- tests/ActivityTest.php
+++ tests/ActivityTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace GroupLife\Core\tests;
 
+use GroupLife\Core\Activity;
 use PHPUnit\Framework\TestCase;
 
 class ActivityTest extends TestCase
--- tests/LeaderTest.php
+++ tests/LeaderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace GroupLife\Core\tests;
 
+use GroupLife\Core\Leader;
 use PHPUnit\Framework\TestCase;
 
 class LeaderTest extends TestCase
