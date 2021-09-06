ae2322fc18a08672e681ff28f98ae366461b5875
Lehersun <kpekep@list.ru>
2021-09-06 13:11:32 +0300

    Remove unnecessery funtionality from Visit object

--- src/Activity/Visit.php
+++ src/Activity/Visit.php
@@ -5,28 +5,17 @@ declare(strict_types=1);
 namespace GroupLife\Core\Activity;
 
 use GroupLife\Core\Activity;
-use GroupLife\Core\Visitor;
 
 class Visit
 {
     private $time;
     private $status;
     private $activity;
-    private $visitor;
 
     public function __construct(\DateTime $time, Activity $activity)
     {
         $this->time = $time;
         $this->activity = $activity;
         $this->status = 'planned';
-    }
-
-    public function createVisit(Visitor $visitor): string
-    {
-        $this->visitor = $visitor;
-        if ($this->time === $this->activity->getTime()) {
-            $this->status = 'confirmed';
-        }
-        return $this->status;
     }
 }
--- tests/Activity/VisitTest.php
+++ tests/Activity/VisitTest.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace GroupLife\Core\tests\Activity;
 
 use GroupLife\Core\Activity;
-use GroupLife\Core\Visitor;
 use PHPUnit\Framework\TestCase;
 use GroupLife\Core\Activity\Visit;
 
 class VisitTest extends TestCase
 {
-    public function createVisit()
+    public function testCanBeConstructed()
     {
         $visit = new Visit(new \DateTime('2021-01-01 10:00'), new Activity('Swimming'));
-        $this->assertEquals('planned', $visit->createVisit(new Visitor('Ivan')));
+        $this->assertIsObject($visit);
     }
 }
