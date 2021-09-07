aac5a71ee232d90dcd5b1a5686eb8ed712b8fada
Lehersun <kpekep@list.ru>
2021-09-07 20:12:33 +0300

    Fix VisitTest according changes in Visit class

--- tests/Activity/VisitTest.php
+++ tests/Activity/VisitTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace GroupLife\Core\tests\Activity;
 
 use GroupLife\Core\Activity;
+use GroupLife\Core\Visitor;
 use PHPUnit\Framework\TestCase;
 use GroupLife\Core\Activity\Visit;
 
@@ -12,7 +13,7 @@ class VisitTest extends TestCase
 {
     public function testCanBeConstructed()
     {
-        $visit = new Visit(new \DateTime('2021-01-01 10:00'), new Activity('Swimming'));
+        $visit = new Visit(new \DateTime('2021-01-01 10:00'), 'Swimming', new Visitor('Ivan', 'Pupkin'));
         $this->assertIsObject($visit);
     }
 }
