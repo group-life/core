abaf659e2c0882036fbaa71fa4ceeac27ac011d9
Lehersun <kpekep@list.ru>
2021-09-10 00:38:51 +0300

    Add construct timetable functional

--- src/Activity.php
+++ src/Activity.php
@@ -12,11 +12,13 @@ class Activity
 {
     private $name;
     private $schedule;
+    private $leader;
 
     public function __construct(string $name, Schedule $schedule)
     {
         $this->name = $name;
         $this->schedule = $schedule;
+        $this->leader = 'Ivanov';
     }
 
     public function getName(): string
@@ -31,5 +33,17 @@ class Activity
                 $activityVisits[] = new Visit($day, $this, $visitor);
         }
         return $activityVisits;
+    }
+    public function getCalendar(\DateTime $startTime, \DateInterval $period): array
+    {
+        $calendar = [];
+        foreach ($this->schedule->materialize($startTime, $period) as $day) {
+            $activity = new \stdClass();
+            $activity->time = $day;
+            $activity->activity = $this->name;
+            $activity->leader = $this->leader;
+            array_push($calendar, $activity);
+        }
+        return $calendar;
     }
 }
---
+++ src/TimeTable.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GroupLife\Core;
+
+class TimeTable
+{
+    private $activities;
+
+    public function __construct(array $activities)
+    {
+        $this->activities = $activities;
+    }
+
+    /**
+     * @return array
+     */
+    public function constructCalendar(\DateTime $startTime, \DateInterval $period): array
+    {
+        $calendar = [];
+        foreach ($this->activities as $activity) {
+            $calendar = array_merge($calendar, $activity->getCalendar(clone $startTime, $period));
+        }
+        usort($calendar, function ($a, $b) {
+            return ($a->time <=> $b->time);
+        });
+        return $calendar;
+    }
+
+
+}
