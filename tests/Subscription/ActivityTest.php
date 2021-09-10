afef4cfb65f8505d31054557cd0a50bdd2c8a22a
Lehersun <kpekep@list.ru>
2021-09-10 10:14:02 +0300

    Fix ActivityTest

--- tests/Subscription/ActivityTest.php
+++ tests/Subscription/ActivityTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace GroupLife\Core\tests\Subscription;
 
 use GroupLife\Core;
+use GroupLife\Core\Leader;
 use GroupLife\Core\Subscription;
 use PHPUnit\Framework\TestCase;
 
@@ -12,8 +13,9 @@ class ActivityTest extends TestCase
 {
     public function testScheduleActivityGetFunctions()
     {
+        $leader = new Leader('Ivan', 'Ivanov');
         $schedule = new Core\Schedule([new Core\Schedule\WeekdayRule('Sunday', '10:00')]);
-        $activity = new Core\Activity('Chess', $schedule);
+        $activity = new Core\Activity('Chess', $schedule, $leader);
         $purchase = new Subscription\Activity(
             new \DateTime('2021-01-01'),
             new \DateInterval('P1M'),
