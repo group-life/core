dffbb54e260e9e017115c4fe62f64c3fa5ac4d97
Lehersun <kpekep@list.ru>
2021-09-14 02:02:37 +0300

    Fix code style

--- tests/lib/Migrations/Version20210913225128.php
+++ tests/lib/Migrations/Version20210913225128.php
@@ -20,8 +20,17 @@ final class Version20210913225128 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE `schedules` (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE `rules` (id INT AUTO_INCREMENT NOT NULL, rules VARCHAR(255), schedule INT, PRIMARY KEY(id))');
-        $this->addSql('ALTER TABLE `rules` ADD FOREIGNKEY schedule REFERENCES `schedules`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;');
+        $this->addSql('
+            CREATE TABLE `rules` (
+                id INT AUTO_INCREMENT NOT NULL, 
+                rules VARCHAR(255), 
+                schedule INT, 
+                PRIMARY KEY(id))
+                ');
+        $this->addSql('
+            ALTER TABLE `rules` ADD FOREIGNKEY schedule 
+            REFERENCES `schedules`(`id`) 
+            ON DELETE CASCADE ON UPDATE RESTRICT;');
     }
 
     public function down(Schema $schema): void
