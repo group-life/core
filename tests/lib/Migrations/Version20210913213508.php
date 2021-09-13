bdbe84be2371a59c14f5d7e42acf154fa703ac17
Lehersun <kpekep@list.ru>
2021-09-14 01:31:46 +0300

    Create first migration class for Schedule

---
+++ tests/lib/Migrations/Version20210913213508.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GroupLife\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Initialize extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Initialize DB';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE `schedules` (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE `rules` (id INT AUTO_INCREMENT NOT NULL, rules VARCHAR(255), schedule INT, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE `rules` ADD FOREIGNKEY schedule REFERENCES `schedules`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE schedules');
+        $this->addSql('DROP TABLE rules');
+    }
+}
