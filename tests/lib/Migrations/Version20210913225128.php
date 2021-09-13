bc1057104b87b7c7a9e6e4be7a8cbe1ac5b2b2f0
Lehersun <kpekep@list.ru>
2021-09-14 01:56:46 +0300

    Fix travis.yml createDB script

--- tests/lib/Migrations/Version20210913225128.php
+++ tests/lib/Migrations/Version20210913225128.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Initialize extends AbstractMigration
+final class Version20210913225128 extends AbstractMigration
 {
     public function getDescription(): string
     {
