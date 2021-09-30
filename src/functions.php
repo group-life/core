e7287a18deb2238c90c7ae1e262cd8f3fc7b1fc1
Lehersun <kpekep@list.ru>
2021-09-30 23:01:50 +0300

    Delete unnecessary function

--- src/functions.php
+++ src/functions.php
@@ -19,15 +19,3 @@ function getDataArray(object $obj): array
 {
     return json_decode(json_encode($obj), true);
 }
-
-/**
- * @param int $seconds
- * @return DateInterval
- * @throws Exception
- */
-function getDateInterval(int $seconds): DateInterval
-{
-    $day1 = date_create('@0');
-    $day2 = date_create('@0')->add(new DateInterval('PT' . $seconds . 'S'));
-    return $day2->diff($day1, true);
-}
