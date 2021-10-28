63e9c2c47e212530d938778d4af0c75212256648
Lehersun <kpekep@list.ru>
2021-10-28 16:57:21 +0300

    Create tests for new Leader::class functions

--- tests/unit/LeaderTest.php
+++ tests/unit/LeaderTest.php
@@ -24,6 +24,20 @@ class LeaderTest extends TestCase
             }', json_encode(self::leaderIvan(), JSON_PRETTY_PRINT));
     }
 
+    public function testSetName()
+    {
+        $leader = $this->leaderIvan();
+        $leader->setName('Petr');
+        self::assertEquals('Petr Ivanov', $leader->getFullName());
+    }
+
+    public function testSetSurname()
+    {
+        $leader = $this->leaderIvan();
+        $leader->setSurname('Petrov');
+        self::assertEquals('Ivan Petrov', $leader->getFullName());
+    }
+
     private static function leaderIvan(): Leader
     {
         return new Leader('Ivan', 'Ivanov');
